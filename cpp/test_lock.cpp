01e24474f393c000391aeac2caf88702c3bb0c6a
jinghuanxiao <jinghuanxiao@outlook.com>
2018-04-16 18:02:36 +0000

    modified:   cpp/test_lock.cpp

--- cpp/test_lock.cpp
+++ cpp/test_lock.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
 #include<semaphore.h>
-
+#include<pthread.h>
+#include<errno.h>
+#include<cstring>
+#include <unistd.h>
+sem_t semLock;
+void *func(void* arg)
+{
+	sleep(10);
+	std::cout<<"func"<<std::endl;
+	pthread_exit(NULL);
+}
 int main(int argc,char *argv[])
 {
-	std::cout<<"hello world"<<std::endl;
+	pthread_t thread;
+	sem_init(&semLock,0,1);
+	int ret = pthread_create(&thread,NULL,func,NULL);
+	if(ret != 0){
+		printf("err =%s",strerror(errno));
+
+	}
+	pthread_join(thread,NULL);
+	sem_destroy(&semLock);
+	return 0;
 }
+
